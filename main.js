d760bd63b58cba415a4c6f5940aa456a9b1e408e
haydy778 <haymillar@icloud.com>
2023-05-23 21:00:48 +1200

    23 May

---
+++ main.js
@@ -0,0 +1,17 @@
+var navbar = document.getElementById("headerTwo");
+
+  window.onscroll = function(){
+    scrollFunction()
+  };
+
+  function scrollFunction() {
+  
+    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+      navbar.style.position = "fixed";
+      navbar.style.top = "0";
+  
+  } else {
+      navbar.style.position = "relative";
+      navbar.style.top = "0px";
+  }
+}
