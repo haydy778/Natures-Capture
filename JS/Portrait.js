dce810213cb9c28f8262732d3e536ffdef2b1c8e
haydy778 <80439221+haydy778@users.noreply.github.com>
2023-09-15 12:18:43 +1200

    Created all the js files for the product filter

---
+++ JS/Portrait.js
@@ -0,0 +1,41 @@
+import {images} from '../data.js' 
+
+const productGrid = document.getElementById("shop");
+images.map((image, index) => {
+    addImageCard(image, index);
+})
+
+
+function addImageCard(image, index) {
+    if (image.collection.includes('Portrait')) {
+    const product = document.createElement('div');
+    const imageContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const textContainer = document.createElement('div');
+    const name = document.createElement('h1');
+    const price = document.createElement('p');
+    const addcart = document.createElement('button');
+
+    addcart.classList.add('addcart');
+    product.classList.add('product');
+    imageContainer.classList.add('image-product-container');
+    img.classList.add('image-product');
+    textContainer.classList.add('text-price-orientation');
+    price.classList.add('price');
+    name.classList.add('name')
+
+    price.appendChild(document.createTextNode(image.price));
+    name.appendChild(document.createTextNode(image.title));
+	product.id = index;
+    img.src = image.source;
+    // product.id = image.title;
+
+    imageContainer.appendChild(img);
+    textContainer.appendChild(name);
+    textContainer.appendChild(price);
+    product.appendChild(imageContainer);
+    product.appendChild(textContainer);
+    productGrid.appendChild(product);
+    }
+}
+
