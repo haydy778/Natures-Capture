a490134ea28f2a7cec14bc80d0ae60606915251c
haydy778 <80439221+haydy778@users.noreply.github.com>
2023-09-15 12:08:32 +1200

    Created all my html files for the filter page

--- JS/shop.js
+++ JS/shop.js
@@ -1,5 +1,4 @@
-
-import {images} from './data.js'
+import {images} from '../data.js' 
 
 const productGrid = document.getElementById("shop");
 images.map((image, index) => {
@@ -8,11 +7,12 @@ images.map((image, index) => {
 
 
 function addImageCard(image, index) {
-    if (image.collection.includes("All")) {
+    if (image.collection.includes('All')) {
     const product = document.createElement('div');
     const imageContainer = document.createElement('div');
     const img = document.createElement('img');
     const textContainer = document.createElement('div');
+    const name = document.createElement('h1');
     const price = document.createElement('p');
     const addcart = document.createElement('button');
 
@@ -22,19 +22,19 @@ function addImageCard(image, index) {
     img.classList.add('image-product');
     textContainer.classList.add('text-price-orientation');
     price.classList.add('price');
+    name.classList.add('name')
 
     price.appendChild(document.createTextNode(image.price));
-
+    name.appendChild(document.createTextNode(image.title));
 	product.id = index;
     img.src = image.source;
     // product.id = image.title;
 
-    product.appendChild(addcart);
     imageContainer.appendChild(img);
+    textContainer.appendChild(name);
     textContainer.appendChild(price);
     product.appendChild(imageContainer);
     product.appendChild(textContainer);
-
     productGrid.appendChild(product);
     }
 }
